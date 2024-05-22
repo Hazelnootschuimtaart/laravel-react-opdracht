722998c209b00c21e66a867db11e84ffe925d636
Louise <louise.schendelaar98@gmail.com>
2024-05-22 11:28:29 +0200

    Added edit function

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -62,9 +62,17 @@ class AuthorController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Author $author)
+    public function update(Request $request, Author $author): RedirectResponse 
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:45',
+            'email' => 'string|email|max:45|unique:authors,email',
+            'age' => 'integer|min:0|max:125',
+        ]);
+
+        $author->update($validated);
+
+        return redirect(route('authors.index'));
     }
 
     /**
--- resources/js/Components/Author.jsx
+++ resources/js/Components/Author.jsx
@@ -4,10 +4,6 @@ import InputError from '@/Components/InputError';
 import PrimaryButton from '@/Components/PrimaryButton';
 import { useForm, usePage } from '@inertiajs/react';
 
-// OP DROPDOWN STUK ALLE CHIRP DINGEN WEGHALEN
-
-
-
 export default function Author({ author }) {
 
     const { auth } = usePage().props;
@@ -19,27 +15,72 @@ export default function Author({ author }) {
     });
 
     const submit = (e) => {
-
         e.preventDefault();
-
         patch(route('authors.update', author.id), { onSuccess: () => setEditing(false) });
-
     };
 
     return (
         <>
+            <div className="flex">
+                <Dropdown>
+                    <Dropdown.Trigger>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                            </svg>
+                        </button>
+                    </Dropdown.Trigger>
+                    <Dropdown.Content>
+                        <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
+                            Edit
+                        </button>
+                    </Dropdown.Content>
+                </Dropdown>
+                {author.created_at !== author.updated_at && <small className="text-sm text-gray-600"> &middot; edited</small>}
+            </div>
             <div className="p-6 flex space-x-2">
-                <div className="flex-1">
-                    <p className="mt-4 text-lg text-gray-900">{author.name}, {author.email}, {author.age} years old</p>
-                    {author.created_at !== author.updated_at && <small className="text-sm text-gray-600"> &middot; edited</small>}
+                <div>
+                    {editing
+                        ? <form onSubmit={submit}>
+
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={data.name}
+                                onChange={e => setData('name', e.target.value)}
+                                className="block w-full border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                name="author-name"
+                                id="author-name"
+                            />
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                value={data.email}
+                                onChange={e => setData('email', e.target.value)}
+                                className="block w-full mt-3 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                name="author-email"
+                                id="author-email"
+                            />
+                            <input
+                                type="number"
+                                placeholder="Age"
+                                value={data.age}
+                                onChange={e => setData('age', e.target.value)}
+                                className="block mt-3 border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+                                name="author-age"
+                                id="author-age"
+                            />
+                            <InputError message={errors.message} className="mt-2" />
+                            <div className="space-x-2">
+                                <PrimaryButton className="mt-4">Save</PrimaryButton>
+                                <button className="mt-4" onClick={() => { setEditing(false); reset(); clearErrors(); }}>Cancel</button>
+                            </div>
+                        </form>
+                        : <p className="mt-4 text-lg text-gray-900">{author.name}, {author.email}, {author.age} years old</p>
+                    }
                 </div>
             </div>
-            <div>
 
-                <button className="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" onClick={() => setEditing(true)}>
-                    Edit
-                </button>
-            </div>
         </>
 
     )
